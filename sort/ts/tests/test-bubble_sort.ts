409ac81bc671269206ce9ae8d6f469ce71f8d79a
boubacar sow <boubacarsowone@gmail.com>
2026-05-14 00:06:50 +0000

    bubble sort Ts

---
+++ sort/ts/tests/test-bubble_sort.ts
@@ -0,0 +1,6 @@
+import { bubble_sort } from "../src/BubbleSort";
+const sortedData = [3, 15, 28, 42, 56, 71, 89, 94];
+
+const result = bubble_sort(sortedData);
+
+console.log(`tableau trie par bubble sort : ${result}`);
